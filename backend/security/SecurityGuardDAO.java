7fc5f6741958795d9c65dcd6f485ddaa5b7fedbc
nhaatj <nguyenngocnhat2004st@gmail.com>
2024-11-25 17:05:31 +0700

    feat:security guard data access object

--- backend/security/SecurityGuardDAO.java
+++ backend/security/SecurityGuardDAO.java
@@ -15,10 +15,78 @@ public class SecurityGuardDAO {
         return resultSet.next();
     }
 
+    public void getProfile(int id) throws SQLException, ClassNotFoundException {
+        Connection connection = databaseConnector.getConnection();
+        String query = "SELECT * FROM SecurityGuards WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println("Name: " + resultSet.getString("phoneNumber"));
+        }
+    }
+
+    public void updateUsername(int id, String username) throws SQLException, ClassNotFoundException {
+        Connection connection = databaseConnector.getConnection();
+        String query = "UPDATE SecurityGuards SET username = ? WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setInt(2, id);
+        preparedStatement.executeUpdate();
+    }
+
+    public boolean updatePassword(int id, String oldPassword, String newPassword)
+            throws SQLException, ClassNotFoundException {
+        Connection connection = databaseConnector.getConnection();
+        String query = "SELECT password FROM SecurityGuards WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            if (resultSet.getString("password").equals(oldPassword)) {
+                query = "UPDATE SecurityGuards SET password = ? WHERE id = ?";
+                preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setString(1, newPassword);
+                preparedStatement.setInt(2, id);
+                preparedStatement.executeUpdate();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void updatePhoneNumber(int id, String phoneNumber) throws SQLException, ClassNotFoundException {
+        Connection connection = databaseConnector.getConnection();
+        String query = "UPDATE SecurityGuards SET phoneNumber = ? WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, phoneNumber);
+        preparedStatement.setInt(2, id);
+        preparedStatement.executeUpdate();
+    }
+
+    public void updateShift(int id, int shift) throws SQLException, ClassNotFoundException {
+        Connection connection = databaseConnector.getConnection();
+        String query = "UPDATE SecurityGuards SET shift = ? WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setInt(1, shift);
+        preparedStatement.setInt(2, id);
+        preparedStatement.executeUpdate();
+    }
+
+    public void updateGender(int id, String gender) throws SQLException, ClassNotFoundException {
+        Connection connection = databaseConnector.getConnection();
+        String query = "UPDATE SecurityGuards SET gender = ? WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, gender);
+        preparedStatement.setInt(2, id);
+        preparedStatement.executeUpdate();
+    }
+
     public static void main(String[] args) throws ClassNotFoundException {
         SecurityGuardDAO securityGuardDAO = new SecurityGuardDAO();
         try {
             System.out.println(securityGuardDAO.login("admin", "Abc@12345"));
+            securityGuardDAO.updatePassword(1, "Abc@12345", "null");
         } catch (SQLException e) {
             e.printStackTrace();
         }
