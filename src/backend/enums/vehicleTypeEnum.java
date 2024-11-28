2a72000135cd1b6add85b2d3ad093e0025031149
nhaatj <nguyenngocnhat2004st@gmail.com>
2024-11-28 18:13:25 +0700

    feat/vehicle

---
+++ src/backend/enums/vehicleTypeEnum.java
@@ -0,0 +1,5 @@
+package src.backend.enums;
+
+public enum vehicleTypeEnum {
+    CAR, MOTORBIKE, BICYCLE
+}
--- src/backend/ticket/TicketDAO.java
+++ src/backend/ticket/TicketDAO.java
@@ -5,16 +5,5 @@ import java.sql.*;
 import src.backend.databaseConnector;
 
 public class TicketDAO {
-    public void addTicket(Ticket ticket) throws SQLException, ClassNotFoundException {
-        Connection connection = databaseConnector.getConnection();
-        String query = "INSERT INTO Ticket (loaive, giovao, giora, gia) VALUES (?, ?, ?, ?)";
-        PreparedStatement preparedStatement = connection.prepareStatement(query);
-        preparedStatement.setString(1, ticket.getLoaive());
-        preparedStatement.setTimestamp(2, new Timestamp(ticket.getGiovao().getTime()));
-        preparedStatement.setTimestamp(3, new Timestamp(ticket.getGiora().getTime()));
-        preparedStatement.setDouble(4, ticket.getGia());
-        preparedStatement.executeUpdate();
-        connection.close();
-    }
 
 }
